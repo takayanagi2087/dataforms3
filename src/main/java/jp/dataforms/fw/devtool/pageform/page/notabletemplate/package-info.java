9b7cb1facfb6df8f4d39a8a9b8687f24304c7199
takay <takay@daiv4z>
2024-05-06 16:46:01 +0900

    不要ファイルの削除。

--- src/main/java/jp/dataforms/fw/devtool/pageform/page/notabletemplate/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * ページjavaクラス作成用のテンプレート(テーブル無)パッケージ。
- */
-package jp.dataforms.fw.devtool.pageform.page.notabletemplate;
--- src/main/java/jp/dataforms/fw/devtool/pageform/page/template/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- *  ページjavaクラス作成用のテンプレート(テーブル有)パッケージ。
- */
-package jp.dataforms.fw.devtool.pageform.page.template;
