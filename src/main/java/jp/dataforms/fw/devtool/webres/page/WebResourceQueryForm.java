629a6a47b61669c18becca74e82f4ef96345275d
takay <takay@daiv4z>
2024-07-22 20:16:27 +0900

    Pageクラス名の入力時にパッケージ名を更新する。

--- src/main/java/jp/dataforms/fw/devtool/webres/page/WebResourceQueryForm.java
+++ src/main/java/jp/dataforms/fw/devtool/webres/page/WebResourceQueryForm.java
@@ -29,7 +29,7 @@ public class WebResourceQueryForm extends QueryForm {
 //		funcsel.setPackageOption("page");
 		this.addField(funcsel);
 		this.addField(new PackageNameField()).addValidator(new RequiredValidator());
-		this.addField(new PageClassNameField()).setAutocomplete(true);
+		this.addField(new PageClassNameField()).setAutocomplete(true).setRelationDataAcquisition(true);
 		this.addField(new WebComponentTypeListField());
 		this.addField(new FlagField("generatableOnly"));
 		this.addField(new ClassNameField());
