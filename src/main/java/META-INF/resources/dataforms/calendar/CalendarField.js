dd3fcbbaff96d6eabe8df94d782e98d555754634
takay <takay@daiv4z>
2024-10-09 14:17:23 +0900

    カレンダーの選択処理を実装。

--- src/main/java/META-INF/resources/dataforms/calendar/CalendarField.js
+++ src/main/java/META-INF/resources/dataforms/calendar/CalendarField.js
@@ -16,14 +16,34 @@ import { MessagesUtil } from '../util/MessagesUtil.js';
  */
 export class CalendarField extends Field {
 	/**
-	 * 日付の値。
+	 * 年月日。
 	 */
 	#dateValue = null;
+
+	/**
+	 * 年。
+	 */
+	#year = null;
 	
+	/**
+	 * 月。
+	 */
+	#month = null;
+	
+	/**
+	 * 日。
+	 */
+	#day = null;
+		
 	/**
 	 * 日付フォーマット。
 	 */
 	#dateFormat = null;
+	
+	/**
+	 * カレンダー情報。
+	 */
+	#calendarInfo = null;
 	
 	/**
 	 * コンストラクタ。
@@ -38,40 +58,73 @@ export class CalendarField extends Field {
 	 */
 	attach() {
 		super.attach();
-		logger.log("id=" + this.id);
-		logger.log("calendarHtml=" + this.calendarHtml);
-		logger.log("calendar=" + this.realId);
 		this.get().html(this.calendarHtml);
 		for (let i = 0; i < this.weekList.length; i++) {
 			this.find(".week" + i).text(this.weekList[i]);
 		}
+		this.setupTd();
 		this.find(".prevMonthButton").click(() => {
 			this.onPrevMonth();
 		});
 		this.find(".nextMonthButton").click(() => {
 			this.onNextMonth();
 		});
-//		$("#" + this.selectorEscape(this.get().attr("id"))).html(this.calendarHtml);
-//
-// フィールドのイベント処理
-//
-// this.get()でフィールドのjQueryオブジェクトの取得が可能です。
-//
-// 変更時のイベント処理
-/*
-		this.get().change(() => {
-			// TODO:必要な処理を実装してください。
-		});
-*/
+	}
 
-// クリック時のイベント処理
-/*
-		this.get().click(() => {
-			// TODO:必要な処理を実装してください。
+	/**
+	 * 各TDのイベント処理を登録します。
+	 */
+	setupTd() {
+		let idx = 0;
+		this.find("table.calendarTable td").each((_, td) => {
+			$(td).attr("data-index", idx);
+			$(td).attr("class", "dateCell");
+			$(td).click((ev) => {
+				this.onClickCell(ev);
+			});
+			idx++;
 		});
-*/
+	}
+
+	/**
+	 * 日付を選択します。
+	 * @param {Date} d 日付。
+	 */
+	selectDate(d) {
+		this.#year = d.getFullYear();
+		this.#month = d.getMonth();
+		this.#day = d.getDate();
+		this.find("table.calendarTable td").each((_, td) => {
+			let day = parseInt($(td).data("day"));
+			if (this.#day == day) {
+				$(td).addClass("selected");
+			} else {
+				$(td).removeClass("selected");
+			}			
+		});
+	}
+		
+	/**
+	 * カレンダーのセルのクリックイベント処理。
+	 * @param {Event} ev イベント処理。
+	 */
+	onClickCell(ev) {
+		logger.log("ev=", ev);
+		let idx = $(ev.currentTarget).data("index");
+		logger.log("idx=" + idx);
+		let dateInfo = this.#calendarInfo["dateInfo" + idx];
+		if (dateInfo != null) {
+			let day = $(ev.currentTarget).data("day");
+			logger.log("date=" + day);
+			let date = new Date(this.#year, this.#month, parseInt(day));
+			let fmt = new SimpleDateFormat(this.#dateFormat);
+			this.#dateValue	= fmt.format(date);
+			logger.log("#dateValue=" + this.#dateValue);
+			this.selectDate(date);
+		}
 	}
 	
+		
 	/**
 	 * 前の月を表示。
 	 */
@@ -95,15 +148,29 @@ export class CalendarField extends Field {
 	}
 
 	/**
+	 * 日付毎の表示内容を取得します。
+	 * <pre>
+	 * このメソッドをオーバーライドしてアプリケーション毎の表示内容を作成してください。
+	 * </pre>
+	 * @param {Object} dateInfo 日付毎の情報。
+	 */
+	getDateContents(dateInfo) {
+		logger.log("dateInfo=", dateInfo);
+		return "";
+	}
+	
+	
+	/**
 	 * 日付セルに設定するHTMLテキストを取得します。
 	 * @param {Number} idx セルインデックス。
 	 * @param {Object} dateInfo 日付情報。
 	 */	
 	getDateCellHtml(idx, dateInfo) {
-		let tag = '<span data-id="date' + idx + '" class="date">' + dateInfo.day + '</span><br/>';
+		let tag = '<div data-id="date' + idx + '" class="date">' + dateInfo.day + '</div>';
+		let contents = this.getDateContents(dateInfo);
+		tag += '<div class="contents">' + contents + '</div>';
 		return tag;
 	}
-	
 	
 	/**
 	 * 日付を設定します。
@@ -111,72 +178,46 @@ export class CalendarField extends Field {
 	 */
 	async setValue(v) {
 		this.#dateValue = v;
-		logger.log("date=" + this.#dateValue);
+		let fmt = new SimpleDateFormat(this.#dateFormat);
+		let d = fmt.parse(this.#dateValue);
+		this.#year = d.getFullYear();
+		this.#month = d.getMonth();
+		this.#day = d.getDate();
 		let m = this.getWebMethod("getCalenderInfo");
 		let r = await m.execute("date=" + this.#dateValue);
-		logger.log("r=", r);
 		if (r.status == JsonResponse.SUCCESS) {
 			this.find(".monthYear").text(r.result.monthYear);
+			this.#calendarInfo = r.result;
 			let idx = 0;
 			this.find("table.calendarTable tbody tr").each((_, tr) => {
 				$(tr).find("td").each((_, td) => {
-					let dateInfo = r.result["dateInfo" + idx];
+					let dateInfo = this.#calendarInfo["dateInfo" + idx];
+					logger.log("dateInfo=", dateInfo);
 					if (dateInfo != null) {
 						let tag = this.getDateCellHtml(idx, dateInfo);
 						$(td).html(tag);
+						$(td).attr("data-day", dateInfo.day);
 					} else {
 						$(td).html("");
+						$(td).attr("data-day", null);
 					}
 					idx++;
 				});
 			});
-
 		}
 	}
 	
-	
-
-	// 独自のWebメソッドを呼び出す場合は、以下のコメントを参考にしてください。
 	/**
-	 * Webメソッドの呼び出しサンプル。
-	 *
+	 * 値を取得します。
+	 * @return {Date} 値の取得。 
 	 */
-/*
-	async callWebMethod() {
-		try {
-			let method = this.getWebMethod("webMethod");
-			let r = await method.execute(this.id + "=" + this.getValue());
-			// TODO:応答情報を適切に処理してください。
-			logger.dir(r);
-		} catch (e) {
-			currentPage.reportError(e);
+	getValue() {
+		if (this.#dateValue != null) {
+			let fmt = new SimpleDateFormat(this.#dateFormat);
+			return fmt.parse(this.#dateValue);			
+		} else {
+			return null;
 		}
 	}
-*/
-
-	// フィールドの各種動作をカスタマイズするには以下のメソッドをオーバーライドしてください。
-	/**
-	 * autocompleteの選択時の処理を記述します。
-	 */
-/*
-	onAutocompleteSelected() {
-		super.onAutocompleteSelected();
-	}
-*/
-
-	/**
-	 * 関連データの更新後に呼び出されるメソッドです。
-	 */
-/*
-	onUpdateRelationField() {
-		super.onUpdateRelationField();
-		// このフィールドが配置されたフォームのメソッドを呼び出す場合は以下の様にします。
-		let form = this.getParentForm();
-		if (typeof(form.hogeFunc) == "function") {
-			form.hogeFunc();
-		}
-	}
-*/
-
 }
 
