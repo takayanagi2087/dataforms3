30653bd8bd7ed8424bbe6080426c388e2a9ad0e7
takay <takay@ryzenai>
2026-08-10 11:13:45 +0900

    コメントの誤記を修正。

--- src/main/java/META-INF/resources/dataforms/controller/EditForm.js
+++ src/main/java/META-INF/resources/dataforms/controller/EditForm.js
@@ -15,7 +15,7 @@ import { Form } from './Form.js';
  * <pre>
  * データ編集を行うフォームです。
  * </pre>
- * @extends TableUpdateForm
+ * @extends Form
  *
  * @prop {String} mode "edit"(フォームが編集可能な状態)または"confirm"(フォーム全体が編集不可の状態)の値を取ります。
  * @prop {String} saveMode "new"(新規データの入力中)または"update"(既存データの編集中)の値を取ります。
